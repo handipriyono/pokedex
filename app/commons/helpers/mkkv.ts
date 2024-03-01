fd0f6f0ba46d5563dde96ccc55f064ca8ceaa095
Handi Priyono <handimacbook@gmail.com>
2024-03-01 17:01:29 +0700

    fix: edit

--- app/commons/helpers/mkkv.ts
+++ app/commons/helpers/mkkv.ts
@@ -1,3 +1,11 @@
 import { MMKV } from "react-native-mmkv";
 
 export const storage = new MMKV();
+
+//use below on expo go
+// const storages = {
+//   set: (key: string, value: any) => {},
+//   get: (key: string) => {},
+// };
+
+// export const storage = storages;
