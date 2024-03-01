2aaa10ccb4b02eb67ecc369c046e8630046d3c2d
Handi Priyono <handimacbook@gmail.com>
2024-03-01 13:23:00 +0700

    feat: add homepage page init page pokedex list

---
+++ app/screens/Homepage/index.tsx
@@ -0,0 +1,36 @@
+import { View, Text, TouchableOpacity, SafeAreaView } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import SearchBox from "./components/SearchBox";
+import PokemonList from "../../commons/components/ListPokemon";
+import Styles from "./style";
+import useGetPokemonList from "./hooks/useGetPokemonList";
+import useSearch from "./hooks/useSearch";
+
+type THomePage = {
+  route: any;
+  navigation: () => void;
+};
+
+function HomePage({ route, navigation }: THomePage) {
+  const { search, onSearch, debouncedSearch } = useSearch();
+  const { data, fetchNextPage } = useGetPokemonList({
+    search,
+    debouncedSearch,
+  });
+
+  return (
+    <SafeAreaView style={Styles.safeArea}>
+      <View style={Styles.homeWrapper}>
+        <SearchBox search={search} onSearch={onSearch} />
+        <PokemonList
+          fetchNextPage={fetchNextPage}
+          navigation={navigation}
+          data={data}
+        />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default HomePage;
---
+++ app/screens/Homepage/style.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  homeWrapper: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingBottom: 10,
+  },
+  safeArea: { flex: 1, backgroundColor: "#fff" },
+  searchBoxWrapper: {
+    paddingTop: 10,
+    paddingBottom: 0,
+    marginBottom: 5,
+    paddingHorizontal: 5,
+  },
+  textInput: {
+    backgroundColor: "#fff",
+    height: 40,
+    borderColor: "#bbbbbb",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+  },
+});
