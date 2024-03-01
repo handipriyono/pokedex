5bd76f941f21bc899279534391560495bbba2829
Handi Priyono <handimacbook@gmail.com>
2024-03-01 13:23:51 +0700

    feat: add hooks getDetail pokemon

---
+++ app/screens/DetailPokemon/hooks/useGetDetailPokemon.ts
@@ -0,0 +1,45 @@
+import { getDetailPokemon } from "../api";
+import { useQuery } from "@tanstack/react-query";
+import { useCallback } from "react";
+
+const queryKey = "get-detail-pokemon";
+
+type TParamsDetailPokemon = {
+  id: string;
+};
+
+const useGetDetailPokemon = ({ id }: TParamsDetailPokemon) => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: [queryKey, id],
+    queryFn: () => getDetailPokemon({ id }),
+  });
+
+  const getSpritesValues = () => {
+    try {
+      return Object?.values(data?.sprites)
+        ?.filter((x) => x)
+        ?.filter((x) => typeof x === "string");
+    } catch (error) {
+      return [];
+    }
+  };
+
+  const getAbilities = () => {
+    try {
+      return data?.abilities?.map((x: any) => x?.ability?.name);
+    } catch (error) {
+      return [];
+    }
+  };
+
+  return {
+    data,
+    isLoading,
+    isError,
+    getSpritesValues,
+    getAbilities,
+    name: data?.name,
+  };
+};
+
+export default useGetDetailPokemon;
