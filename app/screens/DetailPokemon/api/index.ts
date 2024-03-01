0eccabdf8b0d5fa93ca1ed76799fc2201371f9c8
Handi Priyono <handimacbook@gmail.com>
2024-03-01 13:23:19 +0700

    feat: add APi detail pokemon

---
+++ app/screens/DetailPokemon/api/index.ts
@@ -0,0 +1,14 @@
+import Axios from "../../../commons/helpers/axios";
+
+type TDetailAPI = {
+  id: string;
+};
+
+export const getDetailPokemon = async ({ id }: TDetailAPI) => {
+  try {
+    const response = await Axios.get(`/api/v2/pokemon/${id}`);
+    return response?.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
