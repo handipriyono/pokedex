c6ba1eabbad8fe3dc7daa456aab1d9bd5cc14fa1
Handi Priyono <handimacbook@gmail.com>
2024-05-15 09:13:57 +0700

    feat: add queryclient config adjustment

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
-import Navigation from "./app/navigations";
+import Navigation from "@navigations";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // default: true
+    },
+  },
+});
 
 export default function App() {
   return (
