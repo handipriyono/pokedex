c1bafed9ae77461db6b366c57a667c1389d81ca2
Handi Priyono <handimacbook@gmail.com>
2024-05-15 09:15:18 +0700

    feat: add abs path import

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@commons": "./app/commons",
+            "@screens": "./app/screens",
+            "@navigations": "./app/navigations",
+            // Add other aliases as needed
+          },
+        },
+      ],
+    ],
   };
 };
